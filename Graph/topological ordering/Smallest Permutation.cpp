2a9625029d80a8a024dc191b1014d9c41baae020
preetsagar <ps792001@gmail.com>
2024-08-25 14:43:40 +0530

    Smallest Permutaion in Kahn's Algo

--- Graph/topological ordering/Smallest Permutation.cpp
+++ Graph/topological ordering/Smallest Permutation.cpp
@@ -1,3 +1,41 @@
-/*
+#include <bits/stdc++.h>
+using namespace std;
 
-*/
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> graph(n + 1,vector<int>());
+    vector<int> indeg(n + 1,0);
+
+    for(int i = 0; i < m; i++){
+        int a,b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        indeg[b]++;
+    }
+
+    // kahn's algo
+    priority_queue<int> pq;
+    vector<int> topo;
+    for(int i = 1; i <= n; i++){
+        if(indeg[i] == 0) pq.push(-i);
+    }
+    while(!pq.empty()){
+        int u = -pq.top(); pq.pop();
+        topo.push_back(u);
+
+        for(int v : graph[u]){
+            indeg[v]--;
+            if(indeg[v] == 0){
+                pq.push(-v);
+            }
+        }
+    }
+    // print lexographically smallest topo sort
+    if(topo.size() == n)
+        for(int i : topo) cout << i << " ";
+    else
+        cout << "-1";
+
+    return 0;
+}
