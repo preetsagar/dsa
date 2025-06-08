efd4c7416b5a1e398e36fe3c1e4af02ef690348f
preetsagar <ps792001@gmail.com>
2025-06-08 19:20:50 +0530

    Bellman Ford Algo

---
+++ cohort/11. Graph/10. Bellman Ford/Bellman-Ford Revisited.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int n,m;
+vector<pair<int, pair<int, int>>> edges;
+vector<int> dis;
+
+signed main() {
+    cin>>n>>m;
+    dis.resize(n+1, 1e18);
+    for(int i=0; i<m; i++){
+        int u,v,w; cin>>u>>v>>w;
+        edges.push_back({u, {v, -w}});
+    }
+    dis[1]=0;
+    for(int i=1; i<=n; i++){
+        for(auto j:edges){
+            int u=j.first, v=j.second.first, w=j.second.second;
+            if(dis[v] > dis[u]+w){
+                dis[v] = dis[u]+w;
+            }
+        }
+    }
+
+    for(auto j:edges){
+        int u=j.first, v=j.second.first, w=j.second.second;
+        if(dis[u]!=1e8 && dis[v]!=1e18 && dis[v] > dis[u]+w){
+            cout<<-1;
+            return 0;
+        }
+    }
+
+    cout<<-dis[n];
+
+    return 0;
+}
