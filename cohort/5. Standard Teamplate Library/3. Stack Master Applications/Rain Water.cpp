be44a365cd5b7f89eeac02f3e469741ea32e204f
preetsagar <ps792001@gmail.com>
2026-02-25 00:50:05 +0530

    update

--- cohort/5. Standard Teamplate Library/3. Stack Master Applications/Rain Water.cpp
+++ cohort/5. Standard Teamplate Library/3. Stack Master Applications/Rain Water.cpp
@@ -6,23 +6,25 @@ using namespace std;
 signed main() {
     int t; cin>>t; while(t--){
         int n; cin>>n;
-        vector<int> arr(n),nseL(n),nseR(n);
+        vector<int> arr(n), lb(n), rb(n);
         for(int i=0; i<n; i++) cin>>arr[i];
 
-        nseL[0]=arr[0];
+        lb[0] = arr[0];
         for(int i=1; i<n; i++){
-            nseL[i]=max(arr[i],nseL[i-1]);
+            lb[i] = max(lb[i-1], arr[i]);
         }
-        nseR[n-1]=arr[n-1];
+
+        rb[n-1] = arr[n-1];
         for(int i=n-2; i>=0; i--){
-            nseR[i]=max(arr[i],nseR[i+1]);
+            rb[i] = max(rb[i+1], arr[i]);
         }
 
         int ans=0;
         for(int i=0; i<n; i++){
-            ans+=(min(nseL[i],nseR[i])-arr[i]);
+            int t = min(lb[i], rb[i]) - arr[i];
+            ans += t;
         }
-        cout<<ans<<"\n";
+        cout<<ans<<'\n';
     }
     return 0;
 }
