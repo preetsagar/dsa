66d17d3fb6d1cc1f7ccc0378f932779c95e27998
preetsagar <ps792001@gmail.com>
2026-02-25 01:11:10 +0530

    update

--- cohort/5. Standard Teamplate Library/3. Stack Master Applications/Super Minimum Sum.cpp
+++ cohort/5. Standard Teamplate Library/3. Stack Master Applications/Super Minimum Sum.cpp
@@ -3,28 +3,35 @@ using namespace std;
 
 #define int long long
 
+int n;
+vector<int> arr, pse, nse;
+
 signed main() {
-    int t; cin>>t; while(t--){
-        int n; cin>>n;
-        vector<int> arr(n),nseL(n),nseR(n);
+    int t; cin>>t;  while(t--){
+        cin>>n;
+        arr.resize(n);
+        pse.resize(n);
+        nse.resize(n);
         for(int i=0; i<n; i++) cin>>arr[i];
 
-        for(int i=0; i<n; i++){
-            nseL[i]=i-1;
-            while(nseL[i]!=-1&&arr[i]<=arr[nseL[i]]) nseL[i]=nseL[nseL[i]];
-        }
         for(int i=n-1; i>=0; i--){
-            nseR[i]=i+1;
-            while(nseR[i]!=n&&arr[i]<=arr[nseR[i]]) nseR[i]=nseR[nseR[i]];
+            nse[i] = i+1;
+            while(nse[i]!=n && arr[i] <= arr[nse[i]]) nse[i] = nse[nse[i]];
+        }
+
+        for(int i=0; i<n; i++){
+            pse[i] = i-1;
+            while(pse[i]!=-1 && arr[i] <= arr[pse[i]]) pse[i] = pse[pse[i]];
         }
 
         int ans=0;
         for(int i=0; i<n; i++){
-            int lhs=i-nseL[i];
-            int rhs=nseR[i]-i;
-            ans+=arr[i]*(lhs*rhs);
+            int l = i-pse[i];
+            int r = nse[i]-i;
+
+            ans += arr[i]*l*r;
         }
-        cout<<ans<<"\n";
+        cout<<ans<<'\n';
     }
     return 0;
 }
