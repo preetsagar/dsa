ae8f8b91b486e41d749856c448bc6471501ef917
preetsagar <ps792001@gmail.com>
2026-03-14 17:33:40 +0530

    update

--- cohort/8. 2 pointers/type 1/Longest Distinct Subarray AZ101.cpp
+++ cohort/8. 2 pointers/type 1/Longest Distinct Subarray AZ101.cpp
@@ -3,31 +3,38 @@ using namespace std;
 
 #define int long long
 
-signed main(){
-    int t; cin>>t; while(t--){
-        int n;  cin>>n;
-        vector<int> arr(n);
-        for(int i=0; i<n; i++) cin>>arr[i];
+int n;
+vector<int> arr;
 
-        int tail=0,head=-1;
-        int ans=0;
-        map<int,int> mp;
-        while(tail<n){
-            while(head+1<n&&mp[arr[head+1]]==0){
-                head++;
-                mp[arr[head]]++;
-            }
-            ans=max(ans,head-tail+1);
-            if(tail>head){
-                tail++;
-                head=tail-1;
-            }
-            else{
-                mp[arr[tail]]--;
-                tail++;
-            }
+int solve(){
+    int tail=0, head=-1;
+    int ans=0, overallAns=0;
+    map<int, int> mp;
+    while(tail <n){
+        while(head+1 < n && mp.find(arr[head+1])==mp.end()){
+            head++;
+            mp[arr[head]]++;
         }
-        cout<<ans<<"\n";
+        ans = (head-tail+1);
+        overallAns = max(overallAns, ans);
+        if(tail > head){
+            tail++;
+            head = tail-1;
+        }else{
+            mp[arr[tail]]--;
+            if(mp[arr[tail]]==0) mp.erase(arr[tail]);
+            tail++;
+        }
+    }
+    return overallAns;
+}
+
+signed main() {
+    int t; cin>>t; while(t--){
+        cin>>n;
+        arr.resize(n);
+        for(int i=0; i<n; i++) cin>>arr[i];
+        cout<<solve()<<"\n";
     }
     return 0;
 }
