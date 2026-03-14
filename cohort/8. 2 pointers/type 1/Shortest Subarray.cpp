356efee8f0beb7325786ee15185457468ad911af
preetsagar <ps792001@gmail.com>
2026-03-14 19:13:56 +0530

    update

--- cohort/8. 2 pointers/type 1/Shortest Subarray.cpp
+++ cohort/8. 2 pointers/type 1/Shortest Subarray.cpp
@@ -1,41 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int freq[100010]={0};
+#define int long long
 
-int main(){
+int n,k;
+vector<int> arr;
+
+int solve(){
+    int tail=0, head=-1;
+    int ans, overallAns=n;
+    map<int, int> mp;
+    while(tail <n){
+        while(head+1 < n && mp.size()<k){
+            head++;
+            mp[arr[head]]++;
+        }
+        if(mp.size()==k){
+            ans = (head-tail+1);
+            overallAns = min(overallAns, ans);
+        }
+        if(tail > head){
+            tail++;
+            head = tail-1;
+        }else{
+            mp[arr[tail]]--;
+            if(mp[arr[tail]]==0) mp.erase(arr[tail]);
+            tail++;
+        }
+    }
+    return overallAns;
+}
+
+signed main() {
     int t; cin>>t; while(t--){
-        int n; cin>>n;
-        vector<int> arr(n);
-        set<int> st;
+        cin>>n;
+        arr.resize(n);
+        map<int, int> mp;
         for(int i=0; i<n; i++){
             cin>>arr[i];
-            st.insert(arr[i]);
+            mp[arr[i]]++;
         }
-        int totalDistEle=st.size();
-
-        int tail=0,head=-1,distEle=0;
-        int ans=n;
-        while(tail<n){
-            while(head+1<n&&distEle<totalDistEle){
-                head++;
-                freq[arr[head]]++;
-                if(freq[arr[head]]==1) distEle++;
-            }
-            if(distEle==totalDistEle){
-                ans=min(ans,head-tail+1);
-            }
-            if(tail>head){
-                tail++;
-                head=tail-1;
-            }
-            else{
-                freq[arr[tail]]--;
-                if(freq[arr[tail]]==0) distEle--;
-                tail++;
-            }
-        }
-        cout<<ans<<'\n';
+        k = mp.size();
+        cout<<solve()<<"\n";
     }
     return 0;
 }
